b6e7fa733a358bdfcf5067a574c9517b2fd2d61e
Joshua Baskaran <joshbaskaran@gmail.com>
2024-03-08 21:45:29 +0530

    Update navigation menu with dropdown component for topics.
    Additionally, also added a toggle with switch logos based on light/dark modes and skeleton component for future loading states to avoid layout shifts.

---
+++ src/components/logo-mode-toggle.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import React from 'react';
+
+export function LogoModeToggle() {
+  const { resolvedTheme } = useTheme();
+  let src;
+
+  switch (resolvedTheme) {
+    case 'light':
+      src = '/pathogens_portal_norway_logo.png';
+      break;
+    case 'dark':
+      src = '/pathogens_portal_norway_logo.png'; // replace with appropriate image
+      break;
+    default:
+      src = '/pathogens_portal_norway_logo.png'; // default to the light image
+      break;
+  }
+
+  return (
+    <Image
+      src={src}
+      alt='Logo of pathogens portal norway'
+      width={150}
+      height={10}
+      className={cn('hover:ring-2 hover:ring-primary')}
+    />
+  );
+}
