981b76304c3ed9b948c76d0a1e177ff7723d4f51
Joshua Baskaran <joshbaskaran@gmail.com>
2024-03-08 15:58:31 +0530

    Feat: light/dark mode toggle.
    Implements a toggleable mode switch using the shadcn/ui button and dropdown menu ui elements. Adds the corresponding radix components to the project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from 'next/font/google';
 
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-porivder';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -21,8 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        {children}
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
