5a1e1263f16e5e5c2f5f41fcf81aaaa7b177e61f
Joshua Baskaran <joshbaskaran@gmail.com>
2024-03-18 12:06:11 +0100

    Fix spelling typo.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from 'next/font/google';
 
 import './globals.css';
 import { cn } from '@/lib/utils';
-import { ThemeProvider } from '@/components/theme-porivder';
+import { ThemeProvider } from '@/components/theme-provider';
 import { NavBar } from '@/components/nav-bar';
 
 const fontSans = FontSans({
