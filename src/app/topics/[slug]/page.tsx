57bc47d3ad4b62738e1e9052ff87ec2e195bf9cf
Joshua Baskaran <joshbaskaran@gmail.com>
2024-03-20 11:51:05 +0100

    Scaffold topics page rendering.
    This needs to be crafted once templates for topics is fixed.

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,11 @@
 import Image from 'next/image';
+import {
+  BlockObjectResponse,
+  ChildPageBlockObjectResponse,
+  ColumnBlockObjectResponse,
+  Heading1BlockObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints';
+import { isFullBlock } from '@notionhq/client';
 
 import { notion } from '@/lib/utils';
 
@@ -9,7 +16,7 @@ export default async function Topics({ params }: { params: { slug: string } }) {
     return null;
   }
 
-  const response = await notion.databases.query({
+  const dbResponse = await notion.databases.query({
     database_id: dbId,
     filter: {
       property: 'slug',
@@ -21,50 +28,42 @@ export default async function Topics({ params }: { params: { slug: string } }) {
     },
   });
 
-  const page = response.results[0];
+  const page = dbResponse.results[0];
+
   const pageBlocks = await notion.blocks.children.list({
     block_id: page.id,
   });
+  const pageBlocksResponse = pageBlocks.results;
 
-  const results = pageBlocks.results as any;
-  const title = results[0].heading_1.rich_text[0].plain_text;
-  const columnsBlock = await notion.blocks.children.list({
-    block_id: results[1].id,
-  });
-  const column1 = await notion.blocks.children.list({
-    block_id: columnsBlock.results[0].id,
-  });
-  const column2 = await notion.blocks.children.list({
-    block_id: columnsBlock.results[1].id,
-  });
-  const column3 = await notion.blocks.children.list({
-    block_id: columnsBlock.results[2].id,
-  });
-  const columns = [column1, column2, column3];
+  // const h1Block = pageBlocksResponse.find(
+  //   it => isFullBlock(it) && it.type === 'heading_1'
+  // ) as Heading1BlockObjectResponse | undefined;
+  // if (!h1Block) {
+  //   throw new Error('Missing H1');
+  // }
+  // const title = h1Block.heading_1.rich_text[0].plain_text;
+
+  // const columnsBlock = pageBlocksResponse.find(
+  //   it => isFullBlock(it) && it.type === 'column_list'
+  // ) as ColumnBlockObjectResponse | undefined;
+  // if (!columnsBlock) {
+  //   throw new Error('Missing columns');
+  // }
+
+  // const columnsResponse = await notion.blocks.children.list({
+  //   block_id: columnsBlock.id,
+  // });
+  // const columns = columnsResponse.results as ColumnBlockObjectResponse[];
+  // const columnChildrenIds = columns.map(it => it.id);
+  // const columnChildrenPromises = columnChildrenIds.map(it =>
+  //   notion.blocks.children.list({ block_id: it })
+  // );
+
+  // const columnChildren = await Promise.all(columnChildrenPromises);
 
   return (
     <div>
-      {/* <pre>{JSON.stringify(pageBlocks, null, 4)}</pre> */}
-      <h1 className='text-2xl font-bold'>{title}</h1>
-      <div className='flex flex-row justify-between'>
-        {columns.map((it: any) => (
-          <div key={it.results[0].id}>
-            <h2 className='text-lg font-semibold'>
-              {it.results[0].heading_2.rich_text[0].plain_text}
-            </h2>
-            <p>{}</p>
-            <h3>{it.results[0].id}</h3>
-            <p>{it.results[1].paragraph.rich_text[0].plain_text}</p>
-            <Image
-              src={it.results[2].image.file.url}
-              alt=''
-              width={200}
-              height={200}
-            />
-            {/* <pre>{JSON.stringify(it.results[2].image.file.url, null, 2)}</pre> */}
-          </div>
-        ))}
-      </div>
+      <pre>{JSON.stringify(pageBlocksResponse, null, 4)}</pre>
     </div>
   );
 }
