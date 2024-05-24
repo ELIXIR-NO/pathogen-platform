2c48c04d91181c64f964f69e553f48f3c840b185
Joshua Baskaran <joshbaskaran@gmail.com>
2024-05-24 15:13:52 +0200

    Scaffold home page.
    Includes a search bar UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+import { SearchBar } from '@/components/search-bar';
+import { Separator } from '@/components/ui/separator';
+import { Info } from 'lucide-react';
+
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main className='flex w-full flex-col gap-y-3'>
+      <div className='text-center text-3xl font-bold'>
+        Pathogen Portal Norway
+      </div>
+      <div className='text-justify'>
+        The portal provides information about available datasets, resources,
+        tools, and services related to pandemic preparedness in Norway. The
+        portal gives researchers, clinicians and policymakers access to an
+        extensive collection of biomolecular data about pathogens. The Portal
+        will also be the main access to the Norwegian Pathogen Data Hub which
+        enables sharing pathogen data and their associated
+        clinical/epidemiological metadata under controlled access.
+      </div>
+      <div className='flex flex-row rounded-lg bg-accent p-2'>
+        <div className='flex'>
+          <Info className='self-center stroke-primary' size={20} />
+        </div>
+        <Separator orientation='vertical' className='mx-3 bg-foreground' />
+        <div>
+          We are happy for any input and suggestions for relevant content that
+          you think should be mentioned in the Portal. Contact us at{' '}
+          <a href='contact@elixir.no' className='text-primary'>
+            contact@elixir.no
+          </a>{' '}
+          or use the contact form.
+        </div>
+      </div>
+      <SearchBar />
+    </main>
+  );
 }
