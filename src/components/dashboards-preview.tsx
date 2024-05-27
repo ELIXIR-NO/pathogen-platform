55cec1c27f691924d953602de20dda5d41930b8c
Joshua Baskaran <joshbaskaran@gmail.com>
2024-05-27 10:43:06 +0200

    UI scaffolding for dashboards section.

--- src/components/dashboards-preview.tsx
+++ src/components/dashboards-preview.tsx
@@ -1,50 +1,55 @@
-import { notion } from '@/lib/utils';
+import { getNotionPagesWithTag } from '@/lib/notion-utils';
 import {
-  isFullBlock,
-  isFullPage,
-  isFullPageOrDatabase,
-} from '@notionhq/client';
-import {
-  DatabaseObjectResponse,
-  PageObjectResponse,
-  RichTextItemResponse,
-} from '@notionhq/client/build/src/api-endpoints';
-import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints';
 
 export default async function DashboardsPreview() {
-  const dbId = process.env.NOTION_DB_ID;
-  if (!dbId) {
-    console.log('Notion DB ID not set!! Check env var');
-    return null;
-  }
+  const pandemicPreparednessPages = await getNotionPagesWithTag(
+    'Pandemic preparedness'
+  );
 
-  const dbResponse = await notion.databases.query({
-    database_id: dbId,
-    filter: {
-      property: 'tags',
-      multi_select: {
-        contains: 'Pandemic preparedness',
-      },
-    },
-  });
+  const pandemicPreparednessDashboardItems = getDashboardDetails(
+    pandemicPreparednessPages as PageObjectResponse[]
+  );
 
-  const pages = dbResponse.results;
+  const covidPages = await getNotionPagesWithTag('Covid-19');
+  const covidDashboardItems = getDashboardDetails(
+    covidPages as PageObjectResponse[]
+  );
 
-  const dashboardItems: PageSummary[] = pages.map((page: any) => ({
-    title: page.properties?.page?.title[0]?.plain_text ?? '',
-    tags: page.properties?.tags?.multi_select.map((tag: any) => tag.name) ?? [],
-    slug: page.properties?.slug?.rich_text[0]?.plain_text ?? '',
-    summary: page.properties?.summary?.rich_text[0]?.plain_text ?? '',
-    id: page.id,
-  }));
+  const infectiousDiseasesPages =
+    await getNotionPagesWithTag('Infectious disease');
+  const infectiousDiseasesDashboardItems = getDashboardDetails(
+    infectiousDiseasesPages as PageObjectResponse[]
+  ).slice(0, 3);
 
   return (
     <div className='flex flex-col space-y-4'>
       <div>
         <h2 className='text-2xl font-semibold'>Pandemic Preparedness</h2>
         <div className='flex flex-row space-x-4'>
-          {dashboardItems.map(DashBoardItem)}
-          {/* <pre>{JSON.stringify(pages, null, 4)}</pre> */}
+          {pandemicPreparednessDashboardItems.map(it =>
+            DashBoardItem(it, it.id)
+          )}
+        </div>
+      </div>
+      <div>
+        <h2 className='text-2xl font-semibold'>Covid 19</h2>
+        <div className='flex flex-row space-x-4'>
+          {covidDashboardItems.map(it => DashBoardItem(it, it.id))}
+        </div>
+      </div>
+      <div>
+        <h2 className='text-2xl font-semibold'>Infectious Diseases</h2>
+        <div className='flex flex-row space-x-4'>
+          {infectiousDiseasesDashboardItems.map(it => DashBoardItem(it, it.id))}
         </div>
       </div>
     </div>
@@ -52,22 +57,40 @@ export default async function DashboardsPreview() {
 }
 
 type PageSummary = {
+  id: string;
   title: string;
   tags: string[];
   slug: string;
+  relative_link: string;
   summary: string;
-  id: string;
 };
 
-function DashBoardItem(item: PageSummary) {
+function DashBoardItem(item: PageSummary, key: string) {
   return (
-    <Card>
+    <Card key={key}>
       <CardHeader>
-        <CardTitle>{item.title}</CardTitle>
+        <CardTitle className='text-center capitalize'>{item.title}</CardTitle>
       </CardHeader>
       <CardContent>
-        <p>{item.summary}</p>
+        <p className='text-justify'>{item.summary}</p>
       </CardContent>
+      <CardFooter className='flex flex-row items-center justify-center'>
+        <Button className='relative'>
+          <Link href={`${item.relative_link}/${item.slug}`}>Read More</Link>
+        </Button>
+      </CardFooter>
     </Card>
   );
+}
+
+function getDashboardDetails(page: PageObjectResponse[]): PageSummary[] {
+  return page.map((page: any) => ({
+    id: page.id,
+    title: page.properties?.page?.title[0]?.plain_text ?? '',
+    tags: page.properties?.tags?.multi_select.map((tag: any) => tag.name) ?? [],
+    slug: page.properties?.slug?.rich_text[0]?.plain_text ?? '',
+    relative_link:
+      page.properties?.relative_link?.rich_text[0]?.plain_text ?? '',
+    summary: page.properties?.summary?.rich_text[0]?.plain_text ?? '',
+  }));
 }
