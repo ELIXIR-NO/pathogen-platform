10f4656b32ca1ae0500a948b4ef469e1a2a04523
Joshua Baskaran <joshbaskaran@gmail.com>
2024-06-03 10:43:03 +0200

    Add nextui to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/theme-provider';
 import { NavBar } from '@/components/nav-bar';
+import { NextUIProvider } from '@nextui-org/react';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -30,16 +31,18 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-        >
-          <NavBar />
-          <div className='mx-auto w-3/4 pt-24'>{children}</div>
-        </ThemeProvider>
-        <Analytics />
+        <NextUIProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            <NavBar />
+            <div className='mx-auto w-3/4 pt-24'>{children}</div>
+          </ThemeProvider>
+          <Analytics />
+        </NextUIProvider>
       </body>
     </html>
   );
