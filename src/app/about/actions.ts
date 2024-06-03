8c29d9cbac823d2c0a242768bf08f9ffb86e3676
Joshua Baskaran <joshbaskaran@gmail.com>
2024-06-03 12:52:11 +0200

    Add contact us form UI.
    Requires handling of backend logic to send the email.

---
+++ src/app/about/actions.ts
@@ -0,0 +1,8 @@
+'use server';
+
+import { contactFormSchema } from '@/components/contact-us-form';
+import { z } from 'zod';
+
+export async function onSubmit(values: z.infer<typeof contactFormSchema>) {
+  console.log(values);
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
+import { ContactUsForm } from '@/components/contact-us-form';
+
 export default function About() {
-  return <main>About page</main>;
+  return (
+    <main>
+      <ContactUsForm className='w-1/3' />
+    </main>
+  );
 }
