f14faafc85741a8ad41970e1a6623e40896859ef
Joshua Baskaran <joshbaskaran@gmail.com>
2024-06-03 11:28:56 +0200

    Move providers to separate component.
    Helps with visual clutter and maintenance.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,8 @@ import { Inter as FontSans } from 'next/font/google';
 
 import './globals.css';
 import { cn } from '@/lib/utils';
-import { ThemeProvider } from '@/components/theme-provider';
 import { NavBar } from '@/components/nav-bar';
-import { NextUIProvider } from '@nextui-org/react';
+import { Providers } from '@/app/providers';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -31,18 +30,11 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <NextUIProvider>
-          <ThemeProvider
-            attribute='class'
-            defaultTheme='system'
-            enableSystem
-            disableTransitionOnChange
-          >
-            <NavBar />
-            <div className='mx-auto w-3/4 pt-24'>{children}</div>
-          </ThemeProvider>
+        <Providers>
+          <NavBar />
+          <div className='mx-auto w-3/4 pt-24'>{children}</div>
           <Analytics />
-        </NextUIProvider>
+        </Providers>
       </body>
     </html>
   );
---
+++ src/app/providers.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { ThemeProvider } from '@/components/theme-provider';
+import { NextUIProvider } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <NextUIProvider navigate={router.push}>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </NextUIProvider>
+  );
+}
