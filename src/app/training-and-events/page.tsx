1271dca72d6eefb8be8c52281ce59749200dc8c5
Danilo Lopes Martins <danilolmartins@gmail.com>
2024-08-23 12:18:53 +0200

    update tessDataDiplay and page.tsx for event page

--- src/app/training-and-events/page.tsx
+++ src/app/training-and-events/page.tsx
@@ -1,10 +1,10 @@
 import { TessDataDisplay } from "@/app/training-and-events/tessDataDisplay";
 
 export default function TrainingAndEvents() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Events</h1>
-      <TessDataDisplay />
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-3xl font-bold">Events</h1>
+			<TessDataDisplay />
+		</div>
+	);
 }
