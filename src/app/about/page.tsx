ca27030475baa21ed489824a5b00457908624d2e
Joshua Baskaran <joshbaskaran@gmail.com>
2024-08-28 15:31:58 +0200

    Wrap tabs in a suspense boundary
    This is necessary to be able to use the useSearchParams hook nested within.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,44 +7,47 @@ import {
 import PathogenPortalContributors from "@/app/about/pathogen-portal-contributors";
 import Partners from "@/app/about/partners";
 import ContactUs from "@/app/about/contact-us";
+import { Suspense } from "react";
 
 export default function About() {
 	return (
 		<main>
-			<Tabs defaultValue="people">
-				<TabsList variant="underline">
-					<TabsTrigger
-						value="people"
-						variant="underline"
-						className="text-lg font-semibold text-gray-500 hover:text-foreground"
-					>
-						People
-					</TabsTrigger>
-					<TabsTrigger
-						value="partners"
-						variant="underline"
-						className="text-lg font-semibold text-gray-500 hover:text-foreground"
-					>
-						Partners
-					</TabsTrigger>
-					<TabsTrigger
-						value="contact-us"
-						variant="underline"
-						className="text-lg font-semibold text-gray-500 hover:text-foreground"
-					>
-						Contact Us
-					</TabsTrigger>
-				</TabsList>
-				<TabsContent value="people">
-					<PathogenPortalContributors />
-				</TabsContent>
-				<TabsContent value="partners">
-					<Partners />
-				</TabsContent>
-				<TabsContent value="contact-us">
-					<ContactUs />
-				</TabsContent>
-			</Tabs>
+			<Suspense>
+				<Tabs defaultValue="people">
+					<TabsList variant="underline">
+						<TabsTrigger
+							value="people"
+							variant="underline"
+							className="text-lg font-semibold text-gray-500 hover:text-foreground"
+						>
+							People
+						</TabsTrigger>
+						<TabsTrigger
+							value="partners"
+							variant="underline"
+							className="text-lg font-semibold text-gray-500 hover:text-foreground"
+						>
+							Partners
+						</TabsTrigger>
+						<TabsTrigger
+							value="contact-us"
+							variant="underline"
+							className="text-lg font-semibold text-gray-500 hover:text-foreground"
+						>
+							Contact Us
+						</TabsTrigger>
+					</TabsList>
+					<TabsContent value="people">
+						<PathogenPortalContributors />
+					</TabsContent>
+					<TabsContent value="partners">
+						<Partners />
+					</TabsContent>
+					<TabsContent value="contact-us">
+						<ContactUs />
+					</TabsContent>
+				</Tabs>
+			</Suspense>
 		</main>
 	);
 }
