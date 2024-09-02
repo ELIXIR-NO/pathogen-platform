64ad7c7a5808a28fdb5cb8674574f0a20dd15120
danilomartinss <danilolmartins@gmail.com>
2024-09-02 21:14:52 +0200

    update nav-mobile.tsx

--- src/components/nav-mobile.tsx
+++ src/components/nav-mobile.tsx
@@ -27,17 +27,17 @@ export default function NavMobile() {
 
 	return (
 		<nav className="fixed left-0 right-0 top-0 z-10 h-fit py-2 backdrop-blur-sm">
-    		<div className="container flex flex-row items-center justify-between gap-x-4 pt-2">
-				<div className="border border-gray-200 p-1 rounded">
-          			<Hamburger toggled={menuOpen} toggle={setMenuOpen} />
-        		</div>
-        		<Link href="/" className="mx-auto">
-           			 <LogoModeToggle />
-        		</Link>
-        		<ModeToggle />
-    		</div>
+			<div className="container flex flex-row items-center justify-between gap-x-4 pt-2">
+				<div className="rounded border border-gray-200 p-1">
+					<Hamburger toggled={menuOpen} toggle={setMenuOpen} />
+				</div>
+				<Link href="/" className="mx-auto">
+					<LogoModeToggle />
+				</Link>
+				<ModeToggle />
+			</div>
 			{menuOpen && (
-				<div className="absolute left-0 right-0 top-full shadow-md bg-background">
+				<div className="absolute left-0 right-0 top-full bg-background shadow-md">
 					<ul className="flex flex-col items-start justify-start gap-y-4 p-4">
 						<NavMenuItem
 							title="Topics"
@@ -137,9 +137,9 @@ const NavBarItem: React.FC<NavItem & { onClick: () => void }> = ({
 }) => (
 	<li>
 		<Link
-			className="block w-full text-lg font-normal hover:underline hover:decoration-primary cursor-pointer"
-            href={pageUrl}
-            onClick={onClick}
+			className="block w-full cursor-pointer text-lg font-normal hover:underline hover:decoration-primary"
+			href={pageUrl}
+			onClick={onClick}
 		>
 			{pageName}
 		</Link>
