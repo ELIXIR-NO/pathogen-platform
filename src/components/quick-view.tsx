0549a2c4efcca27e38e4660dd7d8829a3e8b9151
Joshua Baskaran <joshbaskaran@gmail.com>
2024-09-04 13:36:29 +0200

    Increase card titles' font size

--- src/components/quick-view.tsx
+++ src/components/quick-view.tsx
@@ -62,7 +62,7 @@ export default async function QuickView({
 							<Link href={`${item.relativeLink}/${item.slug}`}>
 								<Card className="h-full transition-all duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg">
 									<CardHeader>
-										<CardTitle>{item.title}</CardTitle>
+										<CardTitle className="text-lg">{item.title}</CardTitle>
 									</CardHeader>
 									<CardContent className="flex flex-col">
 										<div className="mb-4 h-48 w-full overflow-hidden rounded-md">
