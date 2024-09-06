c2fe27471664d9def5b43516b04291739bdbaf0e
Joshua Baskaran <joshbaskaran@gmail.com>
2024-09-06 12:56:37 +0200

    Update content in tools and workflows rdm page

--- src/app/rdm/tools-and-workflows/page.tsx
+++ src/app/rdm/tools-and-workflows/page.tsx
@@ -1,13 +1,55 @@
 import ContributorsPanel from "@/components/contributors-panel";
+import CardGrid, { CardGridData } from "@/components/card-grid";
+
+const resources: CardGridData[] = [
+	{
+		title: "usegalaxy.no",
+		description:
+			"The Norwegian Galaxy server is operated by ELIXIR Norway and is free to use for Norwegian researchers. Multiple tools are available for performing analysis of pathogen data.",
+		link: "https://usegalaxy.no/",
+		image: "/logos/usegalaxy.png",
+	},
+	{
+		title: "bio.tools",
+		description:
+			"bio.tools is a comprehensive registry of software and databases maintained by ELIXIR Europe. The registry is indexed and easy to explore to find relevant tools for pathogen data analysis.",
+		link: "https://bio.tools/",
+		image: "/logos/elixir-biotools.png",
+	},
+	{
+		title: "WorkflowHub",
+		description:
+			"WorkflowHub is a registry for describing, sharing and publishing scientific computational workflows. The browser enables easy filtering of workflows relevant to pathogen data analysis.",
+		link: "https://workflowhub.eu/",
+		image: "/logos/workflowhub.png",
+	},
+	{
+		title: "GitHub",
+		description:
+			"GitHub is an online software development platform, used for storing, tracking, and collaborating on software projects. Tools relevant to the analysis of pathogen data that are not yet registered in public tool registries can be found here.",
+		link: "https://github.com/",
+		image: "/logos/GitHub.png",
+	},
+	{
+		title: "usegalaxy.eu",
+		description:
+			"The European Galaxy server is maintained primarily by the Freiburg Galaxy Team. Multiple tools are available for performing analysis of pathogen data.",
+		link: "https://usegalaxy.eu/",
+		image: "/logos/usegalaxy.png",
+	},
+	{
+		title: "microGalaxy",
+		description:
+			"microGalaxy is a community of practice in the Galaxy community and provides a Galaxy server dedicated to Microbial data analysis in Galaxy. Multiple tools are available for performing analysis of pathogen data.",
+		link: "https://microgalaxy.usegalaxy.eu/",
+		image: "/logos/usegalaxy.png",
+	},
+];
 
 export default function ToolsAndWorkflowsPage() {
 	return (
 		<>
-			<div className="fixed right-12 top-32 z-10">
-				<div className="flex flex-col space-y-2">
-					<ContributorsPanel contributors={["dorota", "erik"]} />
-				</div>
-			</div>
+			<ContributorsPanel contributors={["dorota", "erik"]} />
 			<section className="flex flex-col space-y-6 text-justify">
 				<h2 className="text-3xl font-bold">Tools and workflows</h2>
 				<p>
@@ -32,84 +74,8 @@ export default function ToolsAndWorkflowsPage() {
 					or simply would like to get started with their analysis without
 					thinking about installing tools on their own system.
 				</p>
-				<h3 className="text-2xl font-semibold">Useful resources</h3>
-				<h4 className="text-xl font-semibold">usegalaxy.no</h4>
-				<p>
-					The Norwegian Galaxy server is operated by ELIXIR Norway and free to
-					use for Norwegian researchers. Multiple tools are available for
-					performing analysis of pathogen data
-					<a
-						className="text-primary hover:underline"
-						href="https://usegalaxy.no/"
-					>
-						https://usegalaxy.no/
-					</a>
-				</p>
-				<h4 className="text-xl font-semibold">bio.tools</h4>
-				<p>
-					bio.tools is a comprehensive registry of software and databases
-					maintained by ELIXIR Europe. The registry is indexed and easy to
-					explore to find relevant tools for pathogen data analysis
-					<a className="text-primary hover:underline" href="https://bio.tools/">
-						https://bio.tools/
-					</a>
-				</p>
-				<h4 className="text-xl font-semibold">WorkflowHub</h4>
-				<p>
-					WorkflowHub is a registry for describing, sharing and publishing
-					scientific computational workflows. The browser enables easy filtering
-					of workflows relevant for pathogen data analysis
-					<a
-						className="text-primary hover:underline"
-						href="https://workflowhub.eu/"
-					>
-						WorkflowHub
-					</a>
-				</p>
-				<h4 className="text-xl font-semibold">GitHub</h4>
-				<p>
-					GitHub is an online software development platform, used for storing,
-					tracking, and collaborating on software projects. Tools relevant for
-					the analysis of pathogen data that are not yet registered in public
-					tool registries can be found here
-					<a
-						className="text-primary hover:underline"
-						href="https://github.com/"
-					>
-						GitHub: Let’s build from here
-					</a>
-				</p>
-				<h4 className="text-xl font-semibold">usegalaxy.eu</h4>
-				<p>
-					The European Galaxy server is maintained primarily by the Freiburg
-					Galaxy Team. Multiple tools are available for performing analysis of
-					pathogen data
-					<a
-						className="text-primary hover:underline"
-						href="https://usegalaxy.eu/"
-					>
-						https://usegalaxy.eu/
-					</a>
-				</p>
-				<h4 className="text-xl font-semibold">microGalaxy</h4>
-				<p>
-					microGalaxy is a{" "}
-					<a
-						className="text-primary hover:underline"
-						href="https://galaxyproject.org/community/sig/microbial/#microgalaxy-community"
-					>
-						community of practice
-					</a>{" "}
-					in the Galaxy community and provides a Galaxy server dedicated for
-					Microbial data analysis in Galaxy. Multiple tools are available for
-					performing analysis of pathogen data
-					<a
-						className="text-primary hover:underline"
-						href="https://microgalaxy.usegalaxy.eu/"
-					>
-						https://microgalaxy.usegalaxy.eu/
-					</a>
-				</p>
+				<h3 className="text-2xl font-bold">Useful resources</h3>
+				<CardGrid data={resources} />
 			</section>
 		</>
 	);
