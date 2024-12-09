ea34ba5af4f022bfcb736fc7b648ef66bf586dd4
danilomartinss <danilolmartins@gmail.com>
2024-12-09 11:10:26 +0100

    prettier format

--- src/app/dashboards/antibiotic-resistance-and-diseases-dashboards-4/page.tsx
+++ src/app/dashboards/antibiotic-resistance-and-diseases-dashboards-4/page.tsx
@@ -8,31 +8,34 @@ export default function AntibioticResistanceAndDiseasesDashboards3() {
 			</h1>
 			<p>
 				Norsk overvåkingssystem for antibiotikaresistens hos mikrober (
-				<a 
+				<a
 					href="https://www.unn.no/fag-og-forskning/norm-norsk-overvakingssystem-for-antibiotikaresistens-hos-mikrober"
-					className="text-primary hover:underline" 
+					className="text-primary hover:underline"
 				>
 					NORM
-				</a>){" "}
-				is the Norwegian Surveillance System for Antimicrobial Resistance in Microbes. 
-				The health registry collect and analyse data on antibiotic resistance, working 
-				with reference labs to ensure high-quality testing and data interpretation.
+				</a>
+				) is the Norwegian Surveillance System for Antimicrobial Resistance in
+				Microbes. The health registry collect and analyse data on antibiotic
+				resistance, working with reference labs to ensure high-quality testing
+				and data interpretation.
 			</p>
 			<h2 className="text-2xl font-bold">Plotting filters:</h2>
-				<ul className="flex list-decimal flex-col space-y-1 pl-5">
-					<li>
-						<strong>Select Sample Type</strong> - allows only one sample type
-					</li>
-					<li>
-						<strong>Select Microbes</strong> - - allows only one microbe
-					</li>
-					<li>
-						<strong>Select Regions</strong> - allows multiple regions to be displayed as individual lines
-					</li>
-					<li>
-						<strong>Select Antibiotics</strong> - allows multiple antibiotics to be displayed as synchronised plots
-					</li>
-				</ul>
+			<ul className="flex list-decimal flex-col space-y-1 pl-5">
+				<li>
+					<strong>Select Sample Type</strong> - allows only one sample type
+				</li>
+				<li>
+					<strong>Select Microbes</strong> - - allows only one microbe
+				</li>
+				<li>
+					<strong>Select Regions</strong> - allows multiple regions to be
+					displayed as individual lines
+				</li>
+				<li>
+					<strong>Select Antibiotics</strong> - allows multiple antibiotics to
+					be displayed as synchronised plots
+				</li>
+			</ul>
 			<DataLoader />
 			<p className="italic">
 				The data is being fetched from the national health registry{" "}
