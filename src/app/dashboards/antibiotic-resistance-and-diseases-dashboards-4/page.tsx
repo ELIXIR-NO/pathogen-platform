31bb17183cddf119311cd209ca30c998da58e4ff
danilomartinss <danilolmartins@gmail.com>
2024-12-09 11:09:49 +0100

    Updated content to dashboard's page
    - Updated page's header and content

--- src/app/dashboards/antibiotic-resistance-and-diseases-dashboards-4/page.tsx
+++ src/app/dashboards/antibiotic-resistance-and-diseases-dashboards-4/page.tsx
@@ -4,17 +4,35 @@ export default function AntibioticResistanceAndDiseasesDashboards3() {
 	return (
 		<section className="flex w-full flex-col space-y-6 text-justify">
 			<h1 className="text-3xl font-bold">
-				Antimicrobial resistance occurrence
+				Antimicrobial resistance occurrence (NORM)
 			</h1>
 			<p>
-				<a href="https://www.unn.no/fag-og-forskning/norm-norsk-overvakingssystem-for-antibiotikaresistens-hos-mikrober">
+				Norsk overvåkingssystem for antibiotikaresistens hos mikrober (
+				<a 
+					href="https://www.unn.no/fag-og-forskning/norm-norsk-overvakingssystem-for-antibiotikaresistens-hos-mikrober"
+					className="text-primary hover:underline" 
+				>
 					NORM
-				</a>{" "}
-				data from four different isolation sources “Blood”, “Wound”, “Urine” and
-				“Respiratory tract” has been merged. Multiple geographical regions,
-				species and resistance genes can be selected, exemplified in the plot
-				below.
+				</a>){" "}
+				is the Norwegian Surveillance System for Antimicrobial Resistance in Microbes. 
+				The health registry collect and analyse data on antibiotic resistance, working 
+				with reference labs to ensure high-quality testing and data interpretation.
 			</p>
+			<h2 className="text-2xl font-bold">Plotting filters:</h2>
+				<ul className="flex list-decimal flex-col space-y-1 pl-5">
+					<li>
+						<strong>Select Sample Type</strong> - allows only one sample type
+					</li>
+					<li>
+						<strong>Select Microbes</strong> - - allows only one microbe
+					</li>
+					<li>
+						<strong>Select Regions</strong> - allows multiple regions to be displayed as individual lines
+					</li>
+					<li>
+						<strong>Select Antibiotics</strong> - allows multiple antibiotics to be displayed as synchronised plots
+					</li>
+				</ul>
 			<DataLoader />
 			<p className="italic">
 				The data is being fetched from the national health registry{" "}
