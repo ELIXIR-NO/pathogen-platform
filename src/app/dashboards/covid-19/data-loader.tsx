58d670f9da0fd19b93b61e8c61689fdb66e095f2
danilomartinss <danilolmartins@gmail.com>
2024-12-12 14:43:28 +0100

    Prettier write fix

--- src/app/dashboards/covid-19/data-loader.tsx
+++ src/app/dashboards/covid-19/data-loader.tsx
@@ -1,17 +1,9 @@
-import {
-	extractUniqueYears,
-	getCovid19CSVData,
-} from "@/lib/data/csvUtils";
+import { extractUniqueYears, getCovid19CSVData } from "@/lib/data/csvUtils";
 import { Covid19LineChart } from "./multi-region-resistance-line-charts";
 
 export default async function DataLoader() {
 	const records = await getCovid19CSVData();
 	const years = extractUniqueYears(records);
-	
-	return (
-		<Covid19LineChart 
-			years={years}
-			data={records}
-		/>
-	);
-  }
+
+	return <Covid19LineChart years={years} data={records} />;
+}
