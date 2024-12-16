b9c043e5f480e238d43bdc933cb2abefd1e5c3be
danilomartinss <danilolmartins@gmail.com>
2024-12-16 15:01:47 +0100

    Updated csvExport.ts
    - Created a generic funciton(removed NormDataRecord interface)
    - Updated logic to cleanup

--- src/lib/data/csvExport.ts
+++ src/lib/data/csvExport.ts
@@ -1,21 +1,8 @@
-export interface NormDataRecord {
-	ProveAar: string;
-	Opplegg: string;
-	Mikrobe: string;
-	Antibiotika: string;
-	region: string;
-	antall: number;
-	Andel_R: number;
-	Andel_S_I: number;
-	antall_R: number;
-	antall_S_I: number;
-}
-
-const DownloadCSV = (data: NormDataRecord[], fileName: string) => {
-	const convertToCSV = (data: NormDataRecord[]) => {
-		if (!data || data.length === 0) {
-			return "";
-		}
+const DownloadCSV = <T extends Record<string, any>>(data: T[], fileName: string) => {
+    const convertToCSV = (data: T[]) => {
+        if (!data || data.length === 0) {
+            return "";
+        }
 
 		const header = Object.keys(data[0]).join(",");
 		const rows = data.map((row) =>
@@ -26,14 +13,15 @@ const DownloadCSV = (data: NormDataRecord[], fileName: string) => {
 		return [header, ...rows].join("\n");
 	};
 
-	const csvContent = convertToCSV(data);
-	const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-	const url = URL.createObjectURL(blob);
-	const link = document.createElement("a");
-	link.setAttribute("href", url);
-	link.setAttribute("download", `${fileName}.csv`);
-	link.click();
-	URL.revokeObjectURL(url);
+    const csvContent = convertToCSV(data);
+    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', `${fileName}.csv`);
+    link.click();
+    URL.revokeObjectURL(url);
 };
 
 export default DownloadCSV;
+
