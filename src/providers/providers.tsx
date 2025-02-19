a0fcf1db3a8a3bfd8895d245839620f7f80c50db
danilomartinss <danilolmartins@gmail.com>
2025-02-19 13:04:10 +0100

    Updated providers.tsx
    - Updated to prevented ThemeProvider from rendering during SSR to avoid hydration error.

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -2,8 +2,19 @@
 
 import { ThemeProvider } from "@/providers/theme-provider";
 import ReactQueryProvider from "@/providers/react-query-provider";
+import { useState, useEffect } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) {
+		return <>{children}</>; // Render children without ThemeProvider during SSR
+	}
+
 	return (
 		<ThemeProvider
 			attribute="class"
