8604f78fe3a6989f1da7cbdec0b2a3be9888f3f3
Joshua Baskaran <joshbaskaran@gmail.com>
2025-02-27 15:27:44 +0100

    Adjust NormAtlas page layout with full-width container
    - Added wrapper div with negative margin and full-width styling
    - Ensures Atlas component spans the entire screen width

--- src/app/dashboards/norm-atlas/page.tsx
+++ src/app/dashboards/norm-atlas/page.tsx
@@ -4,5 +4,9 @@ import Atlas from "./atlas";
 export default async function NormAtlas() {
 	const data = await getNormAtlasCSVData();
 
-	return <Atlas data={data} />;
+	return (
+		<div className="-mx-[calc((100vw-75vw)/2)] w-screen pl-2 pr-16">
+			<Atlas data={data} />
+		</div>
+	);
 }
