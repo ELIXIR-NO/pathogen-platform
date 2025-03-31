4df0c509fcd1401cda55c951fb1a32ed35620083
danilomartinss <danilolmartins@gmail.com>
2025-03-31 14:33:05 +0200

    added NORM atlas frontpage
    - Added content
    - Added Cards to each dashboard's tab

--- src/app/dashboards/norm-atlas/page.tsx
+++ src/app/dashboards/norm-atlas/page.tsx
@@ -1,44 +1,121 @@
-import { getNormAtlasCSVData } from "@/lib/data/csvUtils";
-import Atlas from "./atlas";
+import CardGrid, { CardGridData } from "@/components/card-grid";
+import { Card, CardContent } from "@/components/ui/card";
 import {
-	Tabs,
-	TabsTrigger,
-	TabsList,
-	TabsContent,
-} from "@/components/ui/tabs-modified";
-import AntibioticResistanceAndDiseasesDashboards3 from "../antibiotic-resistance-and-diseases-dashboards-4/page";
-import { loadGeoJSON } from "@/lib/data/geojsonLoader";
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import Image from "next/image";
 
-export default async function NormAtlas() {
-	const data = await getNormAtlasCSVData();
-	const geoData = await loadGeoJSON();
+const resources: CardGridData[] = [
+	{
+		title: "NORM atlas",
+		description: "",
+		link: "/dashboards/norm-atlas/view?tab=atlas",
+		image: "/logos/map-view.png",
+	},
+	{
+		title: "NORM data trender",
+		description: "",
+		link: "/dashboards/norm-atlas/view?tab=trends",
+		image: "/logos/trends.png",
+	},
+	{
+		title: "NORM side på UNN",
+		description:
+			"Norsk overvåkingssystem for antibiotikaresistens hos mikrober.",
+		link: "https://www.unn.no/fag-og-forskning/norm-norsk-overvakingssystem-for-antibiotikaresistens-hos-mikrober",
+		image: "/logos/norm.png",
+	},
+];
 
+export default function NormAtlas() {
 	return (
-		<Tabs defaultValue="atlas" className="w-full">
-			<TabsList variant="underline">
-				<TabsTrigger
-					value="atlas"
-					variant="underline"
-					className="text-lg font-semibold text-gray-500 hover:text-foreground"
+		<section className="flex w-full flex-col space-y-6 text-justify">
+			<h1 className="text-3xl font-bold">NORM-atlas</h1>
+			<div className="w-full">
+				<HoverCard>
+					<HoverCardTrigger asChild>
+						<Image
+							src="/logos/cdc-petri-unsplash.jpg"
+							alt="Kontakt"
+							width={3660}
+							height={2400}
+							className="h-auto max-h-[350px] w-full rounded-md object-cover object-[50%_30%]"
+						/>
+					</HoverCardTrigger>
+					<HoverCardContent className="w-fit">
+						Photo by{" "}
+						<a
+							href="https://unsplash.com/@cdc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+							className="text-primary hover:underline"
+						>
+							CDC
+						</a>{" "}
+						on{" "}
+						<a
+							href="https://unsplash.com/photos/person-holding-black-framed-eyeglasses-tQZ9nTjsQwU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+							className="text-primary hover:underline"
+						>
+							Unsplash
+						</a>
+					</HoverCardContent>
+				</HoverCard>
+			</div>
+
+			<div className="mt-6 grid place-items-center gap-6 pb-4">
+				<p className="mx-auto w-full text-lg">
+					NORM-atlas gir tilgang til databasen i NORM for utvalgte bakteriearter
+					og antibiotika over tid og fordelt på regioner. Verktøyet fremstiller
+					andelen resistente mikrober (R). Dette er andelen av en bestemt
+					bakterietype som normalt er følsom for det aktuelle antibiotikum, men
+					der man ikke lenger kan forvente effekt av behandling på grunn av
+					resistens. Øvrige bakteriestammer kan behandles enten med standarddose
+					(S) eller økt dose av det aktuelle antibiotikum (I).
+				</p>
+			</div>
+
+			<div className="rounded-md bg-gray-100 px-6 pb-6">
+				<CardGrid data={resources} />
+			</div>
+
+			<div className="mt-6 grid w-full place-items-center gap-6">
+				<Card
+					key={"Kontakt"}
+					className="flex h-full w-full flex-col transition-shadow duration-300 hover:shadow-2xl"
 				>
-					Atlas
-				</TabsTrigger>
-				<TabsTrigger
-					value="trends"
-					variant="underline"
-					className="text-lg font-semibold text-gray-500 hover:text-foreground"
-				>
-					Trends
-				</TabsTrigger>
-			</TabsList>
-			<TabsContent value="atlas">
-				<div className="-mx-[calc((100vw-75vw)/2)] mt-10 w-screen pl-2 pr-16">
-					<Atlas data={data} geoData={geoData} />
-				</div>
-			</TabsContent>
-			<TabsContent value="trends" className="mt-10">
-				<AntibioticResistanceAndDiseasesDashboards3 />
-			</TabsContent>
-		</Tabs>
+					<CardContent className="flex flex-col items-center justify-center gap-6 p-6">
+						<div className="flex flex-col items-center justify-center space-y-6 text-center">
+							<p>
+								Kontakt
+								<br />
+								Gunnar Skov Simonsen <br />
+								Telefon 918 48 680 <br />
+								Mer info på{" "}
+								<a
+									href="https://unn.no/fag-og-forskning/norm-norsk-overvakingssystem-for-antibiotikaresistens-hos-mikrober"
+									className="text-primary hover:underline"
+								>
+									NORMs nettsider
+								</a>
+							</p>
+							<Image
+								src={"/logos/unn.png"}
+								alt={"Kontakt"}
+								width={550}
+								height={250}
+								className="py-2"
+							/>
+							<p>
+								Organisasjonsnummer NO 983 974 899 <br />
+								Telefon 07766 <br />
+								Postadresse Universitetssykehuset Nord-Norge HF, postboks 100,
+								9038 Tromsø
+							</p>
+						</div>
+					</CardContent>
+				</Card>
+			</div>
+		</section>
 	);
 }
