e00330201d5dbebd4ec6f9ddfc9f8d5ae8591d55
danilomartinss <danilolmartins@gmail.com>
2025-03-31 14:42:28 +0200

    Minor fixes - dashboards/antibiotic-resistance-and-diseases-dashboards-4/page.tsx
    - Content translated into Norwegian

--- src/app/dashboards/antibiotic-resistance-and-diseases-dashboards-4/page.tsx
+++ src/app/dashboards/antibiotic-resistance-and-diseases-dashboards-4/page.tsx
@@ -3,10 +3,7 @@ import DataLoader from "./data-loader";
 export default function AntibioticResistanceAndDiseasesDashboards3() {
 	return (
 		<section className="flex w-full flex-col space-y-6 text-justify">
-			<h1 className="text-3xl font-bold">
-				Antimicrobial resistance occurrence (NORM) Antimicrobial resistance
-				occurrence (NORM)
-			</h1>
+			<h1 className="text-3xl font-bold">Trends (NORM)</h1>
 			<p>
 				Norsk overvåkingssystem for antibiotikaresistens hos mikrober (
 				<a
@@ -15,37 +12,36 @@ export default function AntibioticResistanceAndDiseasesDashboards3() {
 				>
 					NORM
 				</a>
-				) is the Norwegian Surveillance System for Antimicrobial Resistance in
-				Microbes. The health registry collect and analyse data on antibiotic
-				resistance, working with reference labs to ensure high-quality testing
-				and data interpretation.
+				) er det norske overvåkingssystemet for antibiotikaresistens i mikrober.
+				Helseregisteret samler inn og analyserer data om antibiotikaresistens og
+				samarbeider med referanselaboratorier for å sikre høy kvalitet på
+				testing og datafortolkning.
 			</p>
-			<h2 className="text-2xl font-bold">Plotting filters:</h2>
+			<h2 className="text-2xl font-bold">Plott forekomst av resistens:</h2>
 			<ul className="flex list-decimal flex-col space-y-1 pl-5">
 				<li>
-					<strong>Select Sample Type</strong> - allows only one sample type
+					<strong>Velg prøvetype</strong> - tillater kun én prøvetype
 				</li>
 				<li>
-					<strong>Select Microbes</strong> - - allows only one microbe
+					<strong>Velg mikrober</strong> - tillater kun én mikrobe
 				</li>
 				<li>
-					<strong>Select Regions</strong> - allows multiple regions to be
-					displayed as individual lines
+					<strong>Velg regioner</strong> - tillater flere regioner som vises som
+					individuelle linjer
 				</li>
 				<li>
-					<strong>Select Antibiotics</strong> - allows multiple antibiotics to
-					be displayed as synchronised plots
+					<strong>Velg antibiotika</strong> - tillater flere antibiotika som
+					vises som synkroniserte plott
 				</li>
 			</ul>
 			<DataLoader />
 			<p className="italic">
-				The data is being fetched from the national health registry{" "}
+				Dataene hentes fra det nasjonale helseregisteret{" "}
 				<a
 					href="https://www.unn.no/fag-og-forskning/norm-norsk-overvakingssystem-for-antibiotikaresistens-hos-mikrober"
 					className="text-primary hover:underline"
 				>
-					Norwegian Surveillance System for Antimicrobial Resistance in Microbes
-					(NORM)
+					Norsk overvåkingssystem for antibiotikaresistens hos mikrober (NORM)
 				</a>
 				.
 			</p>
