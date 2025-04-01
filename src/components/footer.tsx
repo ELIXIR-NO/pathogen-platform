6caad961e6d7a6e55226127e747b86b273fe926a
danilomartinss <danilolmartins@gmail.com>
2025-04-01 10:52:30 +0200

    Updated footer.tsx
    - Updated layout
        - show logos first
        - Quick links side by side

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,75 +3,58 @@ import { Separator } from "@/components/ui/separator";
 import { UiTLogoModeToggle } from "./uit-logo-mode-toggle";
 import { ElixirNorwayLogoModeToggle } from "./elixir-norway-logo-mode-toggle";
 import { CreativeCommons } from "lucide-react";
-import { EuCoFoundedLogoModeToggle } from "@/components/eu-co-founded-logo-mode-toggle";
+import { VetinstLogoModeToggle } from "./vetinst-logo-mode-toggle";
+import { FHILogoModeToggle } from "./fhi-logo-mode-toggle";
 
 export default function Footer() {
 	return (
 		<footer className="w-full bg-background text-foreground">
 			<Separator />
-			<div className="container mx-auto w-1/2 px-4 py-8">
-				<div className="grid grid-cols-1 items-start gap-4 md:grid-cols-4">
-					<div>
-						<Link
-							href="https://elixir.no"
-							className="flex flex-col items-center space-y-4 hover:underline md:items-center"
-						>
-							<ElixirNorwayLogoModeToggle />
-							<p className="text-center text-sm text-muted-foreground md:text-center">
-								Advancing life science research through bioinformatics services
-								and resources.
-							</p>
-						</Link>
-					</div>
-					<div className="flex flex-col items-center md:items-center">
-						<h3 className="mb-4 text-lg font-semibold">Quick Links</h3>
-						<ul className="space-y-2 text-sm">
-							<li>
-								<Link href="/about" className="hover:underline">
-									About
-								</Link>
-							</li>
-							<li>
-								<Link href="/privacy-policy" className="hover:underline">
-									Privacy Policy
-								</Link>
-							</li>
-							<li>
-								<Link href="/terms-of-use" className="hover:underline">
-									Terms of Use
-								</Link>
-							</li>
-							<li>
-								<Link href="/how-to-cite-us" className="hover:underline">
-									How to cite us
-								</Link>
-							</li>
-							<li>
-								<Link href="/about?tab=contact-us" className="hover:underline">
-									Contact Us
-								</Link>
-							</li>
-						</ul>
-					</div>
-					<div>
-						<Link
-							href="https://uit.no"
-							className="flex flex-col items-center space-y-4 hover:underline md:items-center"
-						>
-							<UiTLogoModeToggle />
-							<p className="text-center text-sm text-muted-foreground md:text-center">
-								Affiliated with UiT
-								<br />
-								The Arctic University of Norway
-							</p>
-						</Link>
-					</div>
-					<Link
-						href="https://european-union.europa.eu"
-						className="flex flex-col items-center space-y-4 hover:underline md:items-center"
-					>
-						<EuCoFoundedLogoModeToggle />
+			<div className="container mx-auto w-3/4 px-4 py-8">
+				<div className="flex flex-wrap items-center justify-center gap-6">
+					<Link href="https://elixir.no" className="hover:underline">
+						<ElixirNorwayLogoModeToggle />
 					</Link>
+					<Link href="https://uit.no" className="hover:underline">
+						<UiTLogoModeToggle />
+					</Link>
+					<Link href="https://www.fhi.no" className="hover:underline">
+						<FHILogoModeToggle />
+					</Link>
+					<Link href="https://www.vetinst.no" className="hover:underline">
+						<VetinstLogoModeToggle />
+					</Link>
+				</div>
+
+				<div className="mt-8 flex items-center justify-center gap-8 border-t pt-6">
+					<h3 className="text-2xl font-semibold">Quick Links</h3>
+					<ul className="flex flex-wrap items-center gap-4 text-lg">
+						<li>
+							<Link href="/about" className="hover:underline">
+								About
+							</Link>
+						</li>
+						<li>
+							<Link href="/privacy-policy" className="hover:underline">
+								Privacy Policy
+							</Link>
+						</li>
+						<li>
+							<Link href="/terms-of-use" className="hover:underline">
+								Terms of Use
+							</Link>
+						</li>
+						<li>
+							<Link href="/how-to-cite-us" className="hover:underline">
+								How to cite us
+							</Link>
+						</li>
+						<li>
+							<Link href="/about?tab=contact-us" className="hover:underline">
+								Contact Us
+							</Link>
+						</li>
+					</ul>
 				</div>
 
 				<div className="mt-8 flex flex-row items-center justify-center space-x-2 border-t border-gray-200 pt-4 text-center text-xs text-muted-foreground">
