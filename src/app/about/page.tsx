5d1571f5b80439d206cd400b782e69164bc44bf9
danilomartinss <danilolmartins@gmail.com>
2025-04-01 13:40:33 +0200

    Updated /about/page.tsx
    - Added overview tab

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,13 +8,21 @@ import PathogenPortalContributors from "@/app/about/pathogen-portal-contributors
 import Partners from "@/app/about/partners";
 import ContactUs from "@/app/about/contact-us";
 import { Suspense } from "react";
+import Overview from "@/app/about/overview";
 
 export default function About() {
 	return (
 		<>
 			<Suspense>
-				<Tabs defaultValue="people">
+				<Tabs defaultValue="overview">
 					<TabsList variant="underline">
+						<TabsTrigger
+							value="overview"
+							variant="underline"
+							className="text-lg font-semibold text-gray-500 hover:text-foreground"
+						>
+							Overview
+						</TabsTrigger>
 						<TabsTrigger
 							value="people"
 							variant="underline"
@@ -37,6 +45,9 @@ export default function About() {
 							Contact Us
 						</TabsTrigger>
 					</TabsList>
+					<TabsContent value="overview">
+						<Overview />
+					</TabsContent>
 					<TabsContent value="people">
 						<PathogenPortalContributors />
 					</TabsContent>
