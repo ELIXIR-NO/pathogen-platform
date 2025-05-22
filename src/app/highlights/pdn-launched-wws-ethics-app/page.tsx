3b8f5a2935598603937f9677ada3fca6a4e369bf
danilomartinss <danilolmartins@gmail.com>
2025-05-22 13:40:07 +0200

    Created highlights/pdn-launched-wws-ethics-app page
    - Created page content

---
+++ src/app/highlights/pdn-launched-wws-ethics-app/page.tsx
@@ -0,0 +1,78 @@
+import ContributorsPanel from "@/components/contributors-panel";
+import {
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import CentralImage from "@/components/central-image";
+
+export default function EnterococcusPage() {
+	return (
+		<>
+			<ContributorsPanel contributors={["dorota"]} />
+			<section className="flex flex-col space-y-6 text-justify">
+				<HoverCard>
+					<HoverCardTrigger asChild>
+						<CentralImage
+							src="/highlights/pdn-launched-wws-ethics-app/header-image.png"
+							alt="image"
+						/>
+					</HoverCardTrigger>
+				</HoverCard>
+				<h2 className="text-3xl font-bold">
+					New ethical guidance tool for wastewater surveillance
+				</h2>
+
+				<p>
+					As wastewater surveillance efforts scale globally, ethical, legal, and
+					equity issues grow more complex. PDN, in partnership with{" "}
+					<a
+						href="https://www.linkedin.com/company/pha4ge/"
+						className="text-primary hover:underline"
+					>
+						Public Health Alliance for Genomic Epidemiology (PHA4GE)
+					</a>
+					, has launched a practical, scenario-based tool to guide responsible
+					decision-making in WWS.
+				</p>
+				<p>
+					“We wanted to make it easier for people to ask the right questions and
+					uphold ethical and legal standards from the start.” —{" "}
+					<a
+						href="https://www.linkedin.com/in/nicki-tiffin/"
+						className="text-primary hover:underline"
+					>
+						Nicki Tiffin
+					</a>
+					, PDN co-PI,{" "}
+					<a
+						href="https://www.linkedin.com/company/south-african-national-bioinformatics-institute/"
+						className="text-primary hover:underline"
+					>
+						South African National Bioinformatics Institute (SANBI)
+					</a>
+					,
+					<a
+						href="https://www.linkedin.com/company/uwc/"
+						className="text-primary hover:underline"
+					>
+						University of the Western Cape
+					</a>
+				</p>
+				<p>
+					Useful for researchers, implementers, ethics committees, and funders
+				</p>
+				<p>Supports open data, best practices, and equitable benefit sharing</p>
+				<p>Read more and explore the tool:</p>
+				<p>
+					<a
+						href="https://lnkd.in/eqmWX3UZ"
+						className="text-primary hover:underline"
+					>
+						https://lnkd.in/eqmWX3UZ
+					</a>
+				</p>
+			</section>
+		</>
+	);
+}
