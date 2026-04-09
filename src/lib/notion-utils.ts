58cbeb5990aa2322399abbb5cee7c1d2d35e8987
Joshua Baskaran <joshbaskaran@gmail.com>
2026-04-09 13:53:59 +0200

    chore: formatting fix

--- src/lib/notion-utils.ts
+++ src/lib/notion-utils.ts
@@ -1,5 +1,8 @@
 import { Client } from "@notionhq/client";
-import type { DatabaseObjectResponse, PageObjectResponse } from "@notionhq/client";
+import type {
+	DatabaseObjectResponse,
+	PageObjectResponse,
+} from "@notionhq/client";
 
 export const notion = new Client({
 	auth: process.env.NOTION_API_SECRET,
